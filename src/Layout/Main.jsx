02acc8106b87a2c0680bb590d2e9d5790b1cccbb
sumaiya48 <sumaiyaislam202021@gmail.com>
2024-05-22 22:41:12 +0600

    mainfile

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -6,9 +6,9 @@ import Footer from "../Pages/Shared/Footer/Footer";
 const Main = () => {
     return (
         <div className="">
-            <Navbar></Navbar>
+          <div>  <Navbar></Navbar>
             <Outlet></Outlet>
-            <Footer></Footer>
+            <Footer></Footer></div>
         </div>
     );
 };
