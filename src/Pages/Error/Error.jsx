2a4adc033d0865731e447b5745e41e39ff3001ec
sumaiya48 <sumaiyaislam202021@gmail.com>
2024-05-22 19:25:10 +0600

    error-responsive

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,9 +1,13 @@
 const Error = () => {
     return (
       <div className="flex justify-center items-center h-screen">
-        <div className="relative w-xl">
-          <img src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*7KCpGW9_D2rIlNV2JhFspQ.png" alt="" />
-          <button className="btn btn-outline btn-error absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Go To Home</button>
+        <div className="relative">
+          <img
+            src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*7KCpGW9_D2rIlNV2JhFspQ.png"
+            alt=""
+            className="max-w-full"
+          />
+          <button className="btn btn-outline btn-error absolute bottom-8 left-1/2 transform -translate-x-1/2">Go To Home</button>
         </div>
       </div>
     );
