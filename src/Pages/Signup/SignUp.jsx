e8d681a25249cf9e160d96aa5f0a8e52d65d11c5
sumaiya48 <sumaiyaislam202021@gmail.com>
2024-05-22 22:37:11 +0600

    sigup

--- src/Pages/Signup/SignUp.jsx
+++ src/Pages/Signup/SignUp.jsx
@@ -1,35 +1,31 @@
 import { Link } from 'react-router-dom';
-
 import { useContext } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
 
 const SignUp = () => {
-
     const { createUser } = useContext(AuthContext);
 
-    const handleSignUp = event => {
+    const handleSignUp = (event) => {
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, email, password)
-
+        console.log(name, email, password);
 
         createUser(email, password)
-            .then(result => {
+            .then((result) => {
                 const user = result.user;
-                console.log('created user', user)
+                console.log('created user', user);
             })
-            .catch(error => console.log(error))
-
-    }
+            .catch((error) => console.log(error));
+    };
 
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-                <div className="w-1/2 mr-12">
-                   
+            <div className="hero-content flex flex-col lg:flex-row">
+                <div className="w-full lg:w-1/2 mb-8 lg:mb-0 lg:mr-12 flex justify-center">
+                    {/* Placeholder for additional content or image */}
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
@@ -39,25 +35,25 @@ const SignUp = () => {
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                                <input type="text" name="name" placeholder="Name" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                                <input type="text" name="email" placeholder="Email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">photo URL</span>
+                                    <span className="label-text">Photo URL</span>
                                 </label>
-                                <input type="text" name='photo url' placeholder="photo url" className="input input-bordered" />
+                                <input type="text" name="photoUrl" placeholder="Photo URL" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Confirm Password</span>
+                                    <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name="password" placeholder="Password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
@@ -66,8 +62,9 @@ const SignUp = () => {
                                 <input className="btn btn-primary" type="submit" value="Sign Up" />
                             </div>
                         </form>
-                        <p className='my-4 text-center'>Already Have an Account? <Link className='text-orange-600 font-bold' to="/login">Login</Link> </p>
-                       
+                        <p className="my-4 text-center">
+                            Already Have an Account? <Link className="text-orange-600 font-bold" to="/login">Login</Link>
+                        </p>
                     </div>
                 </div>
             </div>
