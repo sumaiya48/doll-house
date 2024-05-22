ae7bdf83e82b7bbc61fabe3eaeaa765bd9ae7098
sumaiya48 <sumaiyaislam202021@gmail.com>
2024-05-22 19:01:30 +0600

    dolldetails

--- src/Pages/dolldetails/DollDetails.jsx
+++ src/Pages/dolldetails/DollDetails.jsx
@@ -2,26 +2,24 @@ import { useLoaderData } from "react-router-dom";
 
 const DollDetails = () => {
   const doll = useLoaderData();
-  const { name,picture, price, rating, quantity, details,  } = doll;
-  return (
-    <div>
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src={picture} className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-2xl font-bold pb-3">{name}</h1>
+  const { name, picture, price, rating, quantity, details } = doll;
 
-      <p className="py-2">Seller Name:alex</p>
-      <p className="py-2">Seller email:alex@gmail.com</p>
-      <p className="py-2">Price:{price}</p>
-      <p className="py-2">Ratings:{rating}</p>
-      <p className="py-2">Available Quantity:{quantity}</p>
-      <p className="py-2">Description:{details}</p>
-      
-    </div>
-  </div>
-</div>
-      
+  return (
+    <div className="px-4 py-6">
+      <div className="max-w-2xl mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <img src={picture} className="max-w-full rounded-lg shadow-lg" alt={name} />
+          <div>
+            <h1 className="text-2xl font-bold pb-3">{name}</h1>
+            <p className="py-2">Seller Name: Alex</p>
+            <p className="py-2">Seller email: alex@gmail.com</p>
+            <p className="py-2">Price: {price}</p>
+            <p className="py-2">Ratings: {rating}</p>
+            <p className="py-2">Available Quantity: {quantity}</p>
+            <p className="py-2">Description: {details}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
