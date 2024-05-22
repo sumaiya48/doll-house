b8b1d2227c5346ff15d0d429d259330cb358c95d
sumaiya48 <sumaiyaislam202021@gmail.com>
2024-05-22 22:54:22 +0600

    banner

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -5,9 +5,7 @@ const Banner = () => {
     <div className="">
       <div
         className="hero min-h-screen bg-cover bg-center"
-        style={{
-          backgroundImage: `url("https://cdn.shopify.com/s/files/1/0602/2617/5224/files/image4_55509859-6739-4ef4-9825-d770c4b1be56_480x480.png?v=1662698179")`
-        }}
+       
       >
         <div className="hero-overlay bg-opacity-40"></div>
         <div className="hero-content text-center text-neutral-content">
